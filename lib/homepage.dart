96e16eec62088e818f2d244cecf65262d29e8aab
SamarthSeth <samarth19seth@gmail.com>
2022-02-09 16:01:19 +0530

    Day 2 Completed

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final String name = "Samarth";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hey, $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
