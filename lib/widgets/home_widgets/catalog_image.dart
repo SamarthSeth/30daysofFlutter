3db83489cb9ae5dc601bb53406d3c2b00f1d109a
SamarthSeth <samarth19seth@gmail.com>
2022-02-16 23:32:33 +0530

    Day 15 Completed

---
+++ lib/widgets/home_widgets/catalog_image.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:velocity_x/velocity_x.dart';
+
+import '../../utils/themes.dart';
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+    ).box.rounded.p8.color(MyTheme.creamColor).make().p16().w40(context);
+  }
+}
